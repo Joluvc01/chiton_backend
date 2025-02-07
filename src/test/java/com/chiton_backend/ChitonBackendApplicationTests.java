c5aaa4b950ee47fa673dd78284a7ccc60351f594
Joluvc01 <joluvc01@gmail.com>
2025-02-06 21:28:09 -0500

    base structure

---
+++ src/test/java/com/chiton_backend/ChitonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiton_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
